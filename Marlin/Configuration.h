135b90d0c8502bb53649a2d190e21ea3f250be6e
VincentWRX <vincent.zijderveld@quicknet.nl>
2020-03-29 19:48:59 +0200

    e-steps changed to 830

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -730,7 +730,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 824 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 830 }
 
 /**
  * Default Max Feed Rate (mm/s)
