a9ec541002150b920b8d9508aa70ae2cebf30dda
VincentWRX <vincent.zijderveld@quicknet.nl>
2020-03-29 19:34:59 +0200

    Bootscreen added

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -956,7 +956,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 38,2, 10, -1,68 }
+#define NOZZLE_TO_PROBE_OFFSET { 38.2, 0, -1.68 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
