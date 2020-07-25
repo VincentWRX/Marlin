51825cd986e34c0c53990ed7bc98db081f07a604
VincentWRX <vincent.zijderveld@quicknet.nl>
2020-07-25 21:29:17 +0200

    Changed offset for Hangtight

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -956,7 +956,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 38.2, 0, -1.68 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 49.0, -1.0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
